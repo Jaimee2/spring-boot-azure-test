25f1e2b84796a217263fd16f73fb3b18453bfc44
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-06-21 08:32:19 +0200

    init

---
+++ src/main/java/com/example/springjavaazure/SpringJavaAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjavaazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaAzureApplication.class, args);
+    }
+
+}
