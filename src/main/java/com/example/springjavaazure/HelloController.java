757f022353f3482ecc84dd31c702483a79ff6bf5
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-06-21 08:44:15 +0200

    log

--- src/main/java/com/example/springjavaazure/HelloController.java
+++ src/main/java/com/example/springjavaazure/HelloController.java
@@ -1,15 +1,18 @@
 package com.example.springjavaazure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
     public String hello() {
+        log.info("HelloController: hello");
         return "Hello World";
     }
 
